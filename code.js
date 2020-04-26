4f912d1633517c8500be1fe06b41f2dbef021b47
Vibhuti Agarwal <agarwal.vibhuti57@gmail.com>
2020-04-26 16:52:08 +0530

    Added cellClickHandler

--- code.js
+++ code.js
@@ -14,19 +14,28 @@ function createTable() {
 	}
 	
 	var table = document.createElement("table");
-	
 	for (var i = 0; i < rows; i++) {
 		var tr = document.createElement("tr");
 		for (var j = 0; j < cols; j++) {
 			var cell = document.createElement("td");
 			cell.setAttribute("id", i + "_" + j);
 			cell.setAttribute("class", "dead");
+			cell.onclick = cellClickHandler;
 			tr.appendChild(cell);
 		}
 		table.appendChild(tr);
-	}
-	
+	}	
 	gridContainer.appendChild(table);
+}
+
+function cellClickHandler() {
+	var classes = this.getAttribute("class");
+	if (classes.indexOf("live") > -1 ) {
+		this.setAttribute("class", "dead");
+	}
+	else {
+		this.setAttribute("class", "live");
+	}
 }
 
 // start everything
