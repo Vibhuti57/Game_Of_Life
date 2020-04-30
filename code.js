c7454e6fff84596ef421f3c4856e2cc285f0a10d
Vibhuti Agarwal <agarwal.vibhuti57@gmail.com>
2020-04-30 22:56:04 +0530

    Added rules for game of life.

--- code.js
+++ code.js
@@ -98,6 +98,66 @@ function startButtonHandler() {
 
 function play() {
 	console.log("Play the game.")
+	computeNextGen();
+}
+
+function computeNextGen() {
+	for (var i = 0; i < rows; i++) {
+		for (var j = 0; j < cols; j++) {
+			applyRules(i, j);
+		}
+	}
+}
+
+function applyRules(row, col) {
+	var numNeighbor = countNeighbors(row, col);
+	if (grid[row][col] == 1) {
+		if (numNeighbor < 2) {
+			// underpopulation
+			nextGrid[row][col] = 0;
+		}
+		else if (numNeighbor == 2 || numNeighbor == 3) {
+			nextGrid[row][col] = 1;
+		}
+		else if (numNeighbor > 3)
+			// overpopulation
+			nextGrid[row][col] = 0;
+	}
+	else if (grid[row][col] == 0) {
+		if (numNeighbor == 3) {
+			//reproduction
+			nextGrid[row][col] = 1;
+		}
+	}
+}
+
+function countNeighbors(row, col) {
+	var count = 0;
+	if (row-1 >= 0 ) {
+		if (grid[row-1][col] == 1) count++;
+	}
+	if (row-1 >= 0 && col-1 >= 0 ) {
+		if (grid[row-1][col-1] == 1) count++;
+	}
+	if (row-1 >= 0 && col+1 < cols ) {
+		if (grid[row-1][col+1] == 1) count++;
+	}
+	if (col-1 >= 0 ) {
+		if (grid[row][col-1] == 1) count++;
+	}
+	if (col+1 < cols ) {
+		if (grid[row][col+1] == 1) count++;
+	}
+	if (row+1 < rows) {
+		if (grid[row+1][col] == 1) count++;
+	}
+	if (row+1 < rows && col-1 >= 0 ) {
+		if (grid[row+1][col-1] == 1) count++;
+	}
+	if (row+1 < rows && col+1 < cols ) {
+		if (grid[row+1][col+1] == 1) count++;
+	}
+	return count;
 }
 
 // start everything
