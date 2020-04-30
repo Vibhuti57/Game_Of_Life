0342fc4d5d3bb5b675f776b5494f491df5b396b5
Vibhuti Agarwal <agarwal.vibhuti57@gmail.com>
2020-04-30 23:46:49 +0530

    Random button functionality

--- code.js
+++ code.js
@@ -97,7 +97,25 @@ function setupControlButtons() {
 	startButton.onclick = startButtonHandler;
 	
 	var clearButton = document.getElementById("clear");
-	clearButton.onclick= clearButtonHandler;
+	clearButton.onclick = clearButtonHandler;
+	
+	var randomButton = document.getElementById("random");
+	randomButton.onclick = randomButtonHandler;
+}
+
+function randomButtonHandler() {
+	if (playing) return;
+	clearButtonHandler();
+	for (var i = 0; i < rows; i++) {
+		for (var j = 0; j < cols; j++) {
+			var isLive = Math.round(Math.random());
+			if (isLive == 1) {
+				var cell = document.getElementById(i + "_" + j);
+				cell.setAttribute("class", "live");
+				grid[i][j] = 1;
+			}
+		}
+	}
 }
 
 function clearButtonHandler() {
